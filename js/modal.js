ba3b129c4066717a5c2d0fae12e59018895e37f9
K1A2 <jckim0307@gmail.com>
2022-08-31 21:38:02 +0900

    완성

--- js/modal.js
+++ js/modal.js
@@ -2,9 +2,33 @@ const body = document.querySelector('body');
 const modal = document.querySelector('.modal');
 
 var page = 1;
+var routPage = 1;
+
 var busPlace = 0;
+var nowBusId = '';
 var busNumber = 0;
+
 var totalCount = 0;
+var totalRouteCount = 0;
+
+var pageNow = 'bus';
+
+function initModal() {
+    page = 1;
+    routPage = 1;
+    busPlace = 0;
+    nowBusId = '';
+    busNumber = 0;
+    totalCount = 0;
+    totalRouteCount = 0;
+    pageNow = 'bus';
+    modalTitle.html('버스 선택');
+    $('.modal-station-list').css('display', 'none');
+    $('.modal-bus-list').css('display', 'flex');
+    $('.text-no').text('취소');
+}
+
+let modalTitle = $('.modal-title')
 
 modal.addEventListener('click', (event) => {
     if (event.target === modal) {
@@ -13,10 +37,20 @@ modal.addEventListener('click', (event) => {
 });
 
 document.querySelector('.modal-button-back').addEventListener('click', (event) => {
-    closeSearchModal();
+    console.log(pageNow);
+    if (pageNow === 'bus') {
+        closeSearchModal();
+    } else {
+        pageNow = 'bus';
+        $('.modal-station-list').css('display', 'none');
+        $('.modal-bus-list').css('display', 'flex');
+        $('.text-no').text('취소');
+        modalTitle.html('버스 선택');
+        $('.modal-station-list').empty();
+    }
 });
 
-$('.modal-scroll-section').on('scroll', function() {
+$('.modal-bus-list').on('scroll', function() {
     if($(this).scrollTop() + $(this).innerHeight() >= $(this)[0].scrollHeight) {
         if (totalCount <= 10 * page) {
             console.log("no more")
@@ -27,9 +61,48 @@ $('.modal-scroll-section').on('scroll', function() {
     }
 })
 
+$('.modal-station-list').on('scroll', function() {
+    if($(this).scrollTop() + $(this).innerHeight() >= $(this)[0].scrollHeight) {
+        console.log('ndfj')
+        if (totalRouteCount <= 25 * routPage) {
+            console.log("no more")
+            return;
+        }
+        routPage += 1
+        requestRouteByBusId(busPlace, nowBusId, routPage);
+    }
+})
+
 $(document).on('click', '.bus-list-item' , function() {
     // alert($(this).children('.bus-list-number').text());
-    alert($(this).attr('busId'));
+    // alert($(this).attr('busId'));
+    if (pageNow == 'bus') {
+        nowBusId = $(this).attr('busId');
+        console.log(nowBusId)
+        console.log($(this).attr('busId'))
+        routPage = 1;
+        totalRouteCount = 0;
+        const color = $(this).children('.bus-list-number').attr('class').split(' ')[1]
+        console.log(color)
+        modalTitle.html('<span class="' + color + '">' + $(this).children('.bus-list-number').text() + '</span>  정류장 선택');
+        $('.modal-station-list').css('display', 'flex');
+        $('.modal-bus-list').css('display', 'none');
+        $('.text-no').text('뒤로가기');
+        pageNow = 'station';
+        requestRouteByBusId(busPlace, nowBusId, routPage);
+    } else {
+        let busNum = modalTitle.children('span').text()
+        const next = $(this).next().children('.bus-list-number');
+        var nextStation = '';
+        nextStation = next.text();
+        if (nextStation === '') {
+            nextStation = '종점'
+        }
+        console.log(nextStation);
+        const color = modalTitle.children('span').attr('class')
+        addBusStation(busPlace, $(this).attr('stationId'), nowBusId, busNum, nextStation, $(this).children('.bus-list-number').text(), color);
+        closeSearchModal()
+    }
 });
 
 function getBusColor(bustype) {
@@ -47,7 +120,9 @@ function getBusColor(bustype) {
 }
 
 function showSearchModal(busPlace, busNumber) {
-    $('.modal-scroll-section').empty();
+    $('.modal-bus-list').empty();
+    $('.modal-station-list').empty();
+    initModal();
     modal.classList.toggle('show');
     if (modal.classList.contains('show')) {
         body.style.overflow = 'hidden';
@@ -61,7 +136,8 @@ function showSearchModal(busPlace, busNumber) {
 }
 
 function closeSearchModal() {
-    $('.modal-scroll-section').empty();
+    $('.modal-bus-list').empty();
+    $('.modal-station-list').empty();
     modal.classList.toggle('show');
     if (!modal.classList.contains('show')) {
         body.style.overflow = 'auto';
@@ -74,17 +150,40 @@ function updateBusNumberList(data, status) {
         console.log(data);
         let busDatas = data["response"]["body"]["items"]["item"]
         totalCount = data["response"]["body"]["totalCount"]
-        if (busDatas.length > 0) {
+        if (busDatas !== undefined) {
             busDatas.forEach(function (el, index) {
                 let item = '<div class="bus-list-item" busId="' + el.routeid + '">'
                 item += '<div class="bus-list-number ' + getBusColor(el.routetp) + '">' + el.routeno + '</div>'
                 item += '<div class="bus-list-section">' + el.startnodenm + ' - ' +  el.endnodenm + '</div>'
                 item += '</div>'
-                $('.modal-scroll-section').append(item);
+                $('.modal-bus-list').append(item);
                 console.log(el.routetp);
             });
         } else {
             totalCount = 0;
+        }
+    } else {
+        alert("api가 응답하지 않습니다. 페이지를 새로고침 해주세요.")
+    }
+}
+
+function updateStationNumberList(data, status) {
+    if (status == "success") {
+        console.log(data);
+        let busDatas = data["response"]["body"]["items"]["item"]
+        totalRouteCount = data["response"]["body"]["totalCount"]
+        if (busDatas !== undefined) {
+            busDatas.forEach(function (el, index) {
+                let item = '<div class="bus-list-item" stationId="' + el.nodeid + '">'
+                // (el.updowncd === '0' ? '(상)' : '(하)')
+                item += '<div class="bus-list-number">' + el.nodenm  + '</div>'
+                // item += '<div class="bus-list-section">' + el.startnodenm + ' - ' +  el.endnodenm + '</div>'
+                item += '</div>'
+                $('.modal-station-list').append(item);
+                console.log(el.routetp);
+            });
+        } else {
+            totalRouteCount = 0;
         }
     } else {
         alert("api가 응답하지 않습니다. 페이지를 새로고침 해주세요.")
--- js/requs_data.js
+++ js/requs_data.js
@@ -2,6 +2,8 @@ const key = "XaxKt+xaXb6phzK0spqQ5oVvT8T1T+zM0DFmIRK3Vlogtig8M1k0SOAqpNLQIbSoEs0
 const defaultUrl = "https://apis.data.go.kr/1613000/BusRouteInfoInqireService";
 const busInformUrl = defaultUrl + "/getCtyCodeList"
 const busNumberInfoUrl = defaultUrl + "/getRouteNoList"
+const busRouteInfoUrl = defaultUrl + "/getRouteAcctoThrghSttnList"
+const busTinmeUrl = "https://apis.data.go.kr/1613000/ArvlInfoInqireService/getSttnAcctoSpcifyRouteBusArvlPrearngeInfoList"
 
 $(document).ready(function() {
     // getPlaceList();
@@ -57,5 +59,78 @@ function requestBusByNumber(busPlace, busNumber, page) {
         cityCode: busPlace, routeNo: busNumber,
         numOfRows: 10, pageNo: page},
         updateBusNumberList
+    );
+}
+
+function requestRouteByBusId(busPlace, busId, page) {
+    console.log(busId)
+    $.get(
+        busRouteInfoUrl,
+        {serviceKey: key, _type: "json",
+        cityCode: busPlace, routeId: busId,
+        numOfRows: 25, pageNo: page},
+        updateStationNumberList
+    );
+}
+
+function addBusStation(placeNo, stationId, routeId, busNum, nextBusStation, nowBusStation, color) {
+    $.get(
+        busTinmeUrl,
+        {serviceKey: key, _type: "json",
+        cityCode: placeNo, routeId: routeId, nodeId: stationId, 
+        numOfRows: 10, pageNo: 1},
+        function(data, status) {
+            if (status == "success") {
+                console.log(data);
+                let busDatas = data["response"]["body"]["items"]["item"]
+                let totalCount = data["response"]["body"]["totalCount"]
+                nextBusStation += ' 방면'
+                if (busDatas !== undefined) {
+                    console.log(busDatas)
+                    if (Array.isArray(busDatas)) {
+                        console.log('list')
+                        let item = '<div class="layout-main layout-card"><div class="bus-title">'
+                        item += '<div class="bus-number bus-number-card ' + color + '">' + busNum + '</div>'
+                        item += '<div class="bus-station">' + nowBusStation + '</div></div>'
+                        item += '<div class="bus-direction">' + nextBusStation + '</div><div class="bus-time-list">'
+                        item += '<div class="bus-time-first bus-time-item"><div class="bus-time-left">' + Math.ceil(busDatas[0].arrtime / 60) + '분</div>'
+                        item += '<div class="bus-statin-left">' + busDatas[0].arrprevstationcnt +'정류장</div>'
+                        item += '<div class="bus-seat-left">' + '' + '</div></div>'
+                        item += '<div class="bus-time-second bus-time-item"><div class="bus-time-left">' + Math.ceil(busDatas[1].arrtime / 60) + '분</div>'
+                        item += '<div class="bus-statin-left">' + busDatas[1].arrprevstationcnt +'정류장</div>'
+                        item += '<div class="bus-seat-left">' + '' + '</div></div></div></div>'
+                        $('.layout-bus-times').append(item);
+                    } else {
+                        let item = '<div class="layout-main layout-card"><div class="bus-title">'
+                        item += '<div class="bus-number bus-number-card ' + color + '">' + busNum + '</div>'
+                        item += '<div class="bus-station">' + nowBusStation + '</div></div>'
+                        item += '<div class="bus-direction">' + nextBusStation + '</div><div class="bus-time-list">'
+                        item += '<div class="bus-time-first bus-time-item"><div class="bus-time-left">' + Math.ceil(busDatas.arrtime / 60) + '분</div>'
+                        item += '<div class="bus-statin-left">' + busDatas.arrprevstationcnt +'정류장</div>'
+                        item += '<div class="bus-seat-left">' + '' + '</div></div>'
+                        item += '<div class="bus-time-second bus-time-item"><div class="bus-time-left"></div>'
+                        item += '<div class="bus-statin-left"></div>'
+                        item += '<div class="bus-seat-left"></div></div></div></div>'
+                        $('.layout-bus-times').append(item);
+                        console.log('finish')
+                    }
+                } else {
+                    let item = '<div class="layout-main layout-card"><div class="bus-title">'
+                    item += '<div class="bus-number bus-number-card ' + color + '">' + busNum + '</div>'
+                    item += '<div class="bus-station">' + nowBusStation + '</div></div>'
+                    item += '<div class="bus-direction">' + nextBusStation + '</div><div class="bus-time-list">'
+                    item += '<div class="bus-time-first bus-time-item"><div class="bus-time-left"> - 분</div>'
+                    item += '<div class="bus-statin-left"> - 정류장</div>'
+                    item += '<div class="bus-seat-left">' + '' + '</div></div>'
+                    item += '<div class="bus-time-second bus-time-item"><div class="bus-time-left"></div>'
+                    item += '<div class="bus-statin-left"></div>'
+                    item += '<div class="bus-seat-left"></div></div></div></div>'
+                    $('.layout-bus-times').append(item);
+                    totalCount = 0;
+                }
+            } else {
+                alert("api가 응답하지 않습니다. 페이지를 새로고침 해주세요.")
+            }
+        }
     );
 }
